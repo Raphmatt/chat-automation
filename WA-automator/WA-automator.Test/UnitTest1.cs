d95ba5ae430ca6f348589c18c337115938242279
Raphmatt <raphael.andermatt@roche.com>
2022-12-19 08:23:51 +0100

    Init Project creation

---
+++ WA-automator/WA-automator.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WA_automator.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ WA-automator/WA-automator/Program.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WA_automator;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
