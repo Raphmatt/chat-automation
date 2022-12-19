ad9f64ea3d916970b8e107bb6cbbada139082985
Raphmatt <raphael.andermatt@roche.com>
2022-12-19 09:04:11 +0100

    Added Connection to whatsapp

--- WA-automator/WA-automator/Program.cs
+++ WA-automator/WA-automator/Program.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace WA_automator;
 
@@ -7,10 +9,13 @@ public class Program
     public static void Main()
     {
         var browsercontroller = new BrowserController();
-        var wait = browsercontroller.GetWait();
-        var driver = browsercontroller.GetDriver();
+        WebDriverWait wait = browsercontroller.GetWait();
+        ChromeDriver driver = browsercontroller.GetDriver();
         
         string baseUrl = "https://web.whatsapp.com/";
         driver.Navigate().GoToUrl(baseUrl);
+        wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+        Console.WriteLine("Page loaded");
+        Console.ReadLine();
     }
 }
