de55da8d013903463633ee7295f3ec96a79c9785
Raphmatt <raphael.andermatt@roche.com>
2022-12-19 08:52:25 +0100

    Added ChromeDriver

--- WA-automator/WA-automator/Program.cs
+++ WA-automator/WA-automator/Program.cs
@@ -1,10 +1,16 @@
-﻿
+﻿using OpenQA.Selenium.Chrome;
+
 namespace WA_automator;
 
 public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");
+        var browsercontroller = new BrowserController();
+        var wait = browsercontroller.GetWait();
+        var driver = browsercontroller.GetDriver();
+        
+        string baseUrl = "https://web.whatsapp.com/";
+        driver.Navigate().GoToUrl(baseUrl);
     }
 }
