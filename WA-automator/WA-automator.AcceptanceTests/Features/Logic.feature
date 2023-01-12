74c67b003c6b688dbaa79fe6ee839411edc400a8
ceylanemr5 <eceylan@online.gibz.ch>
2023-01-12 21:26:23 +0100

    Try Mocking

--- WA-automator/WA-automator.AcceptanceTests/Features/Logic.feature
+++ WA-automator/WA-automator.AcceptanceTests/Features/Logic.feature
@@ -1,6 +1,36 @@
 ﻿Feature: Logic
 
-Scenario: Add two numbers
+Scenario: Send messages
+	Given Type message
+	And Build mock for message
+	When Sending message
+	Then Function should be called
+
+Scenario: Send empty messages
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+Scenario: Authentication
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+	
+Scenario: Send Message without tel. number
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+	
+Scenario: Valid tel. number
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+	
+Scenario: Invalid tel. number
 	Given the first number is 50
 	And the second number is 70
 	When the two numbers are added
--- WA-automator/WA-automator.AcceptanceTests/Steps/LogicSteps.cs
+++ WA-automator/WA-automator.AcceptanceTests/Steps/LogicSteps.cs
@@ -1,12 +1,41 @@
-﻿namespace WA_automator.AcceptanceTests.Steps;
+﻿using OpenQA.Selenium.DevTools.V85.Target;
+
+namespace WA_automator.AcceptanceTests.Steps;
 
 [Binding]
 public sealed class LogicSteps
 {
     private readonly IBrowserController _browserController;
+    private string msg;
+    
 
     public LogicSteps(ScenarioContext scenarioContext, IBrowserController browserController)
     {
-        _browserController = browserController;
+        this._browserController = browserController;
     }
+
+    [Given(@"Type message")]
+    public void GivenTypeMessage()
+    {
+        msg = "Hello, World";
+    }
+    
+    [Given(@"Build mock for message")]
+    public void GivenBuildMockForMessage()
+    {
+        this.Mock = new Mock<IBrowserController>(); 
+    }
+
+    [When(@"Sending message")]
+    public void WhenSendingMessage()
+    {
+        _browserController.SendMessage(msg);
+    }
+
+    [Then(@"Function should be called")]
+    public void ThenFunctionShouldBeCaled()
+    {
+        
+    }
+    
 }
